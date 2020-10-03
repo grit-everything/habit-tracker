f2da14007a864af389d6bc894da4c647b4f739e1
jake_lyu <gomgom3409@gmail.com>
2020-10-03 14:26:02 +1000

    Updates

---
+++ precache-manifest.5ecf0b9f69b71380b108b6eb2be4ff21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac9d8d512e344b3247b0e6b8d8769c23",
+    "url": "/habit-tracker/index.html"
+  },
+  {
+    "revision": "47349c235adae0a2e2e4",
+    "url": "/habit-tracker/static/css/main.2992eb9a.chunk.css"
+  },
+  {
+    "revision": "310399db756767f54835",
+    "url": "/habit-tracker/static/js/2.458eef58.chunk.js"
+  },
+  {
+    "revision": "1ea337ab0f6eaf802a045a4531cad9ed",
+    "url": "/habit-tracker/static/js/2.458eef58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47349c235adae0a2e2e4",
+    "url": "/habit-tracker/static/js/main.2efbe34e.chunk.js"
+  },
+  {
+    "revision": "5d759b3fde7c32f59156",
+    "url": "/habit-tracker/static/js/runtime-main.0e3ad41c.js"
+  }
+]);
